92c6e59849f9494be040dae2c380ce56b3f9643e
Damien Morcellet <dmorcellet@free.fr>
2021-08-15 12:15:54 +0200

    Extract some missing skirmish statistics

--- src/main/java/delta/games/lotro/extractors/skirmishs/SkirmishStatsExtractor.java
+++ src/main/java/delta/games/lotro/extractors/skirmishs/SkirmishStatsExtractor.java
@@ -83,14 +83,24 @@ public class SkirmishStatsExtractor
     // Best time
     Float bestTime=(Float)props.getProperty(getPropertyName("Best_Time",size,level));
     ret.setBestTime((bestTime!=null)?bestTime.floatValue():0.0f);
-    // Unused: Boss_Kills, Boss_Resets
+    // Boss kills
+    Integer bossKills=(Integer)props.getProperty(getPropertyName("Boss_Kills",size,level));
+    ret.setBossKills((bossKills!=null)?bossKills.intValue():0);
+    // Boss resets
+    Integer bossResets=(Integer)props.getProperty(getPropertyName("Boss_Resets",size,level));
+    ret.setBossResets((bossResets!=null)?bossResets.intValue():0);
     // Control Point Taken
     Integer cpTaken=(Integer)props.getProperty(getPropertyName("CP_Flips",size,level));
     ret.setControlPointsTaken((cpTaken!=null)?cpTaken.intValue():0);
     // Skirmishes completed
     Integer completed=(Integer)props.getProperty(getPropertyName("Completions",size,level));
     ret.setSkirmishesCompleted((completed!=null)?completed.intValue():0);
-    // Unused: Defenders_Lost, Defenders_Saved
+    // Defenders lost
+    Integer defendersLost=(Integer)props.getProperty(getPropertyName("Defenders_Lost",size,level));
+    ret.setDefendersLost((defendersLost!=null)?defendersLost.intValue():0);
+    // Defenders saved
+    Integer defendersSaved=(Integer)props.getProperty(getPropertyName("Defenders_Saved",size,level));
+    ret.setDefendersSaved((defendersSaved!=null)?defendersSaved.intValue():0);
     // Lieutenant kills
     Integer lieutenantKills=(Integer)props.getProperty(getPropertyName("Lieutenant_Kills",size,level));
     ret.setLieutenantKills((lieutenantKills!=null)?lieutenantKills.intValue():0);
@@ -101,13 +111,40 @@ public class SkirmishStatsExtractor
     Integer encountersCompleted=(Integer)props.getProperty(getPropertyName("Optionals_Completed",size,level));
     ret.setEncountersCompleted((encountersCompleted!=null)?encountersCompleted.intValue():0);
     // Unused: SM_Gained_BossKills, SP_Gained_CP, SP_Gained_Defenders, SP_Gained_Optionals, SP_Gained_Tokens
+    Integer smGainedBossKills=(Integer)props.getProperty(getPropertyName("SM_Gained_BossKills",size,level));
+    if ((smGainedBossKills!=null) && (smGainedBossKills.intValue()!=0))
+    {
+      LOGGER.warn("Unused SM_Gained_BossKills: "+smGainedBossKills);
+    }
+    Integer SP_Gained_CP=(Integer)props.getProperty(getPropertyName("SP_Gained_CP",size,level));
+    if ((SP_Gained_CP!=null) && (SP_Gained_CP.intValue()!=0))
+    {
+      LOGGER.warn("Unused SP_Gained_CP: "+SP_Gained_CP);
+    }
+    Integer SP_Gained_Defenders=(Integer)props.getProperty(getPropertyName("SP_Gained_Defenders",size,level));
+    if ((SP_Gained_Defenders!=null) && (SP_Gained_Defenders.intValue()!=0))
+    {
+      LOGGER.warn("Unused SP_Gained_Defenders: "+SP_Gained_Defenders);
+    }
+    Integer SP_Gained_Optionals=(Integer)props.getProperty(getPropertyName("SP_Gained_Optionals",size,level));
+    if ((SP_Gained_Optionals!=null) && (SP_Gained_Optionals.intValue()!=0))
+    {
+      LOGGER.warn("Unused SP_Gained_Optionals: "+SP_Gained_Optionals);
+    }
+    Integer SP_Gained_Tokens=(Integer)props.getProperty(getPropertyName("SP_Gained_Tokens",size,level));
+    if ((SP_Gained_Tokens!=null) && (SP_Gained_Tokens.intValue()!=0))
+    {
+      LOGGER.warn("Unused SP_Gained_Tokens: "+SP_Gained_Tokens);
+    }
     // Marks earned
     Integer marksEarned=(Integer)props.getProperty(getPropertyName("SP_Gained_Other",size,level));
     ret.setTotalMarksEarned((marksEarned!=null)?marksEarned.intValue():0);
     // Played time
     Float playedTime=(Float)props.getProperty(getPropertyName("Skirmish_Played_Time",size,level));
     ret.setPlayTime((playedTime!=null)?playedTime.floatValue():0.0f);
-    // Unused: Soldier_Deaths
+    // Soldier deaths
+    Integer soldierDeaths=(Integer)props.getProperty(getPropertyName("Soldier_Deaths",size,level));
+    ret.setSoldiersDeaths((soldierDeaths!=null)?soldierDeaths.intValue():0);
     return ret;
   }
 
