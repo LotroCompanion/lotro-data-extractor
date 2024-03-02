6da5bdbc2db0766baf087873fa7e16acd5ca4dd8
Damien Morcellet <dmorcellet@free.fr>
2024-03-02 22:53:41 +0100

    Better extraction of the racial traits status

--- src/main/java/delta/games/lotro/extractors/traits/RacialTraitsExtractor.java
+++ src/main/java/delta/games/lotro/extractors/traits/RacialTraitsExtractor.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import delta.games.lotro.character.CharacterData;
+import delta.games.lotro.character.races.RaceDescription;
 import delta.games.lotro.character.stats.buffs.BuffInstance;
 import delta.games.lotro.character.stats.buffs.BuffRegistry;
 import delta.games.lotro.character.stats.buffs.BuffsManager;
+import delta.games.lotro.character.status.traits.shared.SlottedTraitsStatus;
 import delta.games.lotro.character.traits.TraitDescription;
 import delta.games.lotro.character.traits.TraitsManager;
+import delta.games.lotro.extractors.traits.TraitsData;
 
 /**
  * Puts racial traits into character data.
@@ -51,5 +54,39 @@ public class RacialTraitsExtractor
         }
       }
     }
+  }
+
+  /**
+   * Build the racial traits status.
+   * @param traits Raw traits data.
+   * @param race Targeted race.
+   * @return A new status.
+   */
+  public SlottedTraitsStatus buildStatus(TraitsData traits, RaceDescription race)
+  {
+    // Update current racial traits
+    SlottedTraitsStatus status=new SlottedTraitsStatus();
+    List<Integer> racialTraits=traits.getRacialTraits();
+    int nbTraits=racialTraits.size();
+    int[] traitIDs=new int[nbTraits];
+    for(int i=0;i<nbTraits;i++)
+    {
+      traitIDs[i]=racialTraits.get(i).intValue();
+    }
+    status.setTraits(traitIDs);
+    // Acquired traits
+    if (race!=null)
+    {
+      List<TraitDescription> earnableTraits=race.getEarnableTraits();
+      for(TraitDescription earnableTrait : earnableTraits)
+      {
+        Integer traitID=Integer.valueOf(earnableTrait.getIdentifier());
+        if (traits.getAcquiredTraits().contains(traitID))
+        {
+          status.addTraitID(traitID.intValue());
+        }
+      }
+    }
+    return status;
   }
 }
