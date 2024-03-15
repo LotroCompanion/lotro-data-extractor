a8c66d44f63aec0d9d3307c79f6f7523d8fa08b0
Damien Morcellet <dmorcellet@free.fr>
2024-03-15 08:46:57 +0100

    Remove useless code

--- src/main/java/delta/games/lotro/extractors/traits/RacialTraitsExtractor.java
+++ src/main/java/delta/games/lotro/extractors/traits/RacialTraitsExtractor.java
@@ -20,42 +20,6 @@ import delta.games.lotro.character.traits.TraitsManager;
  */
 public class RacialTraitsExtractor
 {
-  private static final Logger LOGGER=Logger.getLogger(RacialTraitsExtractor.class);
-
-  /**
-   * Handle slotted racial traits.
-   * @param storage Storage.
-   * @param racialTraitIds Slotted traits IDs for each position (0=not slotted).
-   */
-  public void handleRacialTraits(CharacterData storage, List<Integer> racialTraitIds)
-  {
-    BuffsManager buffsMgr=storage.getBuffs();
-    BuffRegistry buffsRegistry=BuffRegistry.getInstance();
-    TraitsManager traitsMgr=TraitsManager.getInstance();
-    for(Integer racialTraitId : racialTraitIds)
-    {
-      if ((racialTraitId!=null) && (racialTraitId.intValue()!=0))
-      {
-        TraitDescription trait=traitsMgr.getTrait(racialTraitId.intValue());
-        if (trait!=null)
-        {
-          String traitName=trait.getName();
-          LOGGER.debug("Racial trait: "+traitName);
-          String key=String.valueOf(trait.getIdentifier());
-          BuffInstance buffInstance=buffsRegistry.newBuffInstance(key);
-          if (buffInstance!=null)
-          {
-            buffsMgr.addBuff(buffInstance);
-          }
-        }
-        else
-        {
-          LOGGER.warn("Racial trait not found: "+racialTraitId);
-        }
-      }
-    }
-  }
-
   /**
    * Build the racial traits status.
    * @param traits Raw traits data.
